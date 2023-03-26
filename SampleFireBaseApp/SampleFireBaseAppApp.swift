a1667d352ace477500d24f8700eae6432922841b
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-26 16:42:39 +0900

    Initial Commit

---
+++ SampleFireBaseApp/SampleFireBaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFireBaseAppApp.swift
+//  SampleFireBaseApp
+//
+//  Created by 渡邊魁優 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleFireBaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
