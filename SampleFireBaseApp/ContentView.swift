12961e3c756c4c00ec4c4e9b12ddef755ccf0e0e
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-27 16:38:00 +0900

    commit

--- SampleFireBaseApp/ContentView.swift
+++ SampleFireBaseApp/ContentView.swift
@@ -6,16 +6,71 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
 
 struct ContentView: View {
+    @State var message = ""
+    @State var savedMessage = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                TextField("", text: $message)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.leading)
+                Button(action: {
+                    let ref = Database.database().reference()
+                    ref.child("message")
+                        .setValue(message)
+                }) {
+                    Text("保存")
+                }
+            }
+            .padding()
+            
+            VStack {
+                Text("保存されている値 : " + savedMessage)
+                
+                Button(action: {
+                    let ref = Database.database().reference()
+                    ref.child("message")
+                        .getData { (error, snapshot) in
+                            if let error = error {
+                                print("Error getting \(error)")
+                            }
+                            if let result = snapshot {
+                                guard let message = result.value as? String else {
+                                    return
+                                }
+                                self.savedMessage = message
+                            }
+                            else {
+                                print("error")
+                            }
+                        }
+                }) {
+                    Text("更新")
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            let ref = Database.database().reference()
+            ref.child("message")
+                .getData { (error, snapshot) in
+                    if let error = error {
+                        print("Error getting data \(error)")
+                    }
+                    if let result = snapshot {
+                        guard let message = result.value as? String else {
+                            return
+                        }
+                        self.message = message
+                    }
+                    else {
+                        print("No data available")
+                    }
+                }
+        }
     }
 }
 
